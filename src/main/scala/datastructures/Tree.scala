ec8667401bca91954febc886286f37178b55125c
geoffrey.scheller <geoffrey.scheller.1@us.af.mil>
2016-04-18 12:57:24 -0600

    Added size, numLeaves, and maximum methods to Tree companion object. Created test file for Tree.

--- src/main/scala/datastructures/Tree.scala
+++ src/main/scala/datastructures/Tree.scala
@@ -6,5 +6,36 @@ case class Leaf[A](value: A) extends Tree[A]
 case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 object Tree {
-  
+
+//  def apply[A](as: A*): Tree[A] =
+//    if (as.isEmpty) Nil
+//    else Cons(as.head, apply(as.tail: _*))
+
+  // Exercise 3.25
+  /** Return size of a Tree: leaves + branches 
+   *
+   *  Not tail recursive - may become a problem if tree
+   *  is not well balanced
+   */
+  def size[A](tree: Tree[A]): Int =
+    tree match {
+      case Branch(left, right) => size(left) + size(right) + 1
+      case _ => 1
+    }
+
+  /** Return number of leaves in a Tree */
+  def numLeaves[A](tree: Tree[A]): Int =
+    tree match {
+      case Branch(left, right) => numLeaves(left) + numLeaves(right)
+      case _ => 1
+    }
+
+  // Exercise 3.26
+  /** Returns the maximum element in a Tree[Int] */
+  def maximum(tree: Tree[Int]): Int = 
+    tree match {
+      case Branch(left, right) => maximum(left) max maximum(right)
+      case Leaf(ii) => ii
+    }
+
 }
---
+++ src/test/scala/datastructures/TreeTest.scala
@@ -0,0 +1,60 @@
+import grockScala.datastructures._
+
+object TreeTest {
+
+  /** Test package */
+  def main(args: Array[String]) = {
+
+    // Exercise 3.25 - Test size (and numLeaves)
+
+    // Some data to test with
+    val leaf5 = Leaf(5.0): Tree[Double]
+    val leaf42 = Leaf(42.0): Tree[Double]
+    val leafPI = Leaf(Math.PI)  // Type inference works
+    val tree3 = Branch(leaf5, leaf42)
+    val tree5 = Branch(tree3, leafPI)
+    val tree9 = Branch(tree3, tree5)
+    val tree11 = Branch(tree9, leaf42)
+    val tree21 = Branch(tree11, tree9)
+
+    println("Test Tree.size:")
+    print("Tree.size(leaf5) = "); println(Tree.size(leaf5))
+    print("Tree.size(tree3) = "); println(Tree.size(tree3))
+    print("Tree.size(tree5) = "); println(Tree.size(tree5))
+    print("Tree.size(tree9) = "); println(Tree.size(tree9))
+    print("Tree.size(tree11) = "); println(Tree.size(tree11))
+    print("Tree.size(tree21) = "); println(Tree.size(tree21))
+
+    println("\nTest Tree.numLeaves:")
+    print("Tree.numLeaves(leaf5) = "); println(Tree.numLeaves(leaf5))
+    print("Tree.numLeaves(tree3) = "); println(Tree.numLeaves(tree3))
+    print("Tree.numLeaves(tree5) = "); println(Tree.numLeaves(tree5))
+    print("Tree.numLeaves(tree9) = "); println(Tree.numLeaves(tree9))
+    print("Tree.numLeaves(tree11) = "); println(Tree.numLeaves(tree11))
+    print("Tree.numLeaves(tree21) = "); println(Tree.numLeaves(tree21))
+
+    // Exercise 3.26 - Test maximum
+
+    // Some data to test with
+    val ileaf5 = Leaf(5): Tree[Int]
+    val ileaf42 = Leaf(42): Tree[Int]
+    val ileaf37 = Leaf(37): Tree[Int]
+    val itree3 = Branch(ileaf5, ileaf37)
+    val itree5 = Branch(itree3, ileaf37)
+    val itree9 = Branch(itree3, itree5)
+    val itree11 = Branch(itree9, ileaf42)
+    val itree21 = Branch(itree11, itree9)
+
+    println("\nTest Tree.maximum:")
+    print("Tree.maximum(ileaf5) = "); println(Tree.maximum(ileaf5))
+    print("Tree.maximum(itree3) = "); println(Tree.maximum(itree3))
+    print("Tree.maximum(itree5) = "); println(Tree.maximum(itree5))
+    print("Tree.maximum(itree9) = "); println(Tree.maximum(itree9))
+    print("Tree.maximum(itree11) = "); println(Tree.maximum(itree11))
+    print("Tree.maximum(itree21) = "); println(Tree.maximum(itree21))
+ 
+    // See how it displays
+    print("\nitree9 = "); println(itree9)
+
+  }
+}

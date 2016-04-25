7f4c561ecadd8f00402c4b7327ba85ac424479f9
geoffrey.scheller <geoffrey.scheller.1@us.af.mil>
2016-04-25 12:38:41 -0600

    Beginning to implement methods on Option trait.
    1. ": => B" says type is B and non-strictly evaluated.
    2. "B :> A" says B is a supertype of A
    3. Need to use "this" for pattern matching the trait itself. (This
       was not made clear in the book.)

--- src/main/scala/grockScala/errorhandling/Option.scala
+++ src/main/scala/grockScala/errorhandling/Option.scala
@@ -1,7 +1,27 @@
 package grockScala.errorhandling
 
 /** Implement Option ADT to handle error conditons */
-sealed trait Option[+A]
+sealed trait Option[+A] {
+
+  def map[B](f: A => B): Option[B] = this match {
+    case Some(a) => Some(f(a))
+    case None => None
+  }
+    
+/*
+  /** Apply f, which may fail, to the Option, if not none */
+  def flatMap[B](f: A => Option[B]): Option[B]
+
+  /** Return value, if None return default, default nonstrict */
+  def getOrElse[B >: A](default: => B): B
+
+  /** If None, swap with superclass Option, nonstrictly */
+  def orElse[B >: A](default: => Option[B]): Option[B]
+
+  /** Convert Some to None if predicate false */
+  def filter(pred: A => Boolean): Option[A]
+*/
+}
 case class Some[+A](get: A) extends Option[A]
 case object None extends Option[Nothing]
 

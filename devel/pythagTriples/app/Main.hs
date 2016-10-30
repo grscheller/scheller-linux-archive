fa7952e65edd07373fbff73ef6c9802233b4fd25
Geoffrey Scheller <geoffrey@scheller.com>
2016-10-29 21:13:45 -0600

    Project devel/pythagTriples updates. Test suite does something.  Mathematical underpinnings for pythagorian generation algorithms added to develLog.

--- devel/pythagTriples/app/Main.hs
+++ devel/pythagTriples/app/Main.hs
@@ -1,17 +1,22 @@
 module Main where
 
-import PythagTriples
 import System.Environment (getArgs)
+import Data.List (sort)
+import PythagTriples ( pythagTriplesOrdered
+                     , pythagTriplesFast
+                     , sortTriple
+                     , Triple )
 
 main :: IO ()
 main = do
   args <- getArgs
   case args of
-    ["-o", numStr] -> printTriples $ triplesOrdered (read numStr)
-    ["-f", numStr] -> printTriples $ triplesFast (read numStr)
-    ["-h"]         -> putStrLn usageString
-    [numStr]       -> printTriples $ triplesFast (read numStr)
-    _              -> error("\n  Error: Called with incorrect arguments.\n" ++ usageString)
+    ["-o", numStr]  -> printTriples $ triplesOrdered (read numStr)
+    ["-f", numStr]  -> printTriples $ triplesFast (read numStr)
+    ["-fs", numStr] -> printTriples $ sort $ map sortTriple $ triplesFast (read numStr)
+    ["-h"]          -> putStrLn usageString
+    [numStr]        -> printTriples $ triplesFast (read numStr)
+    _               -> error("\n  Error: Called with incorrect arguments.\n" ++ usageString)
 
 printTriples :: [Triple] -> IO()
 printTriples [] = return ()
@@ -30,13 +35,14 @@ usageString = unlines [
   "    where",
   "      number = number of triples to print",
   "    and",
-  "      -f  Use fast algorithm where triples (a, b, c) are such",
-  "          that a is odd, b is even, and a,b,c have no common",
-  "          factors.",
   "      -o  Triples (a, b, c) are printed in lexiconical order,",
   "          that is a < b < c, and a,b,c have no common factors.",
   "          Algorithm does prints all possible b's and c's before",
   "          going onto next a.",
+  "      -f  Use fast algorithm where triples (a, b, c) are such",
+  "          that a is odd, b is even, and a,b,c have no common",
+  "          factors.",
+  "      -fs Use fast algorithm, but sort results a < b < c.",
   "      -h  Print this usage message.",
   "   ",
   "    Pathagorean triples are integers 0 < a, b, c such that",
--- devel/pythagTriples/src/PythagTriples.hs
+++ devel/pythagTriples/src/PythagTriples.hs
@@ -1,10 +1,21 @@
 module PythagTriples
-  ( pythagTriplesOrdered
+  ( printTriples
+  , pythagTriplesOrdered
   , pythagTriplesFast
+  , sortTriple
   , Triple
   ) where
 
 type Triple = (Int, Int, Int)
+
+sortTriple :: Triple -> Triple
+sortTriple triple =
+  case triple of
+    (a, b, c) | b < a -> (b, a, c)
+    _                 -> triple
+
+printTriples :: [Triple] -> IO()
+printTriples triples = mapM_ putStrLn (map show triples)
 
 pythagTriplesFast :: [Triple]
 pythagTriplesFast = [ (a, 2*m*n, c) |
@@ -17,11 +28,11 @@ pythagTriplesFast = [ (a, 2*m*n, c) |
 
 pythagTriplesOrdered :: [Triple]
 pythagTriplesOrdered = [ (a, b, floor.sqrt $ fromIntegral csq) |
-    a <- [3 .. ]
-  , b <- [a+1, a+3 .. ((a*a - 1) `div` 2)]
-  , gcd b a == 1
-  , let csq = a*a + b*b
-  , isPerfectSquare csq ]
-
-isPerfectSquare :: Int -> Bool
-isPerfectSquare n = n == (floor.sqrt $ fromIntegral n)^2
+      a <- [3 .. ]
+    , b <- [a+1, a+3 .. ((a*a - 1) `div` 2)]
+    , gcd b a == 1
+    , let csq = a*a + b*b
+    , isPerfectSquare csq ]
+  where
+    isPerfectSquare :: Int -> Bool
+    isPerfectSquare n = n == (floor.sqrt $ fromIntegral n)^2

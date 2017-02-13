d60028e35f1b2f3fb844cf5875ec62006c9594b4
Geoffrey Scheller <geoffrey@scheller.com>
2017-02-12 19:25:40 -0700

    Finished first Scala syntax example in devel/scalaSyntax.

--- devel/scalaSyntax/README.md
+++ devel/scalaSyntax/README.md
@@ -1,9 +1,11 @@
 # Basic Scala Language Constructs:
 
 ## Scala's "splat" equivalents: splat.scala
-  Languages like Python have a syntactic sugar to
-  pass an array or tuple into a fixed or variable
-  arity function.
+  Languages like Python and Ruby have a syntactic
+  sugar to pass an array or tuple into a fixed or
+  variable-arity function.
+
+  In Python:
   ```
      >>> def f(m,n,p):
      ...   return m*n + p
@@ -14,7 +16,7 @@
      >>> f(*x)
      10
   ```
-  The code in splat object in splat.scala explores
+  The code in the splat object in splat.scala explores
   similar constructs in scala.
 
   To compile the code, 
@@ -25,4 +27,6 @@
   ```
      $ scala scalaSyntax.splat  
   ```
-  Currently, code is just a start.
+  It makes sense to "splat" variable length datastructures into
+  variadic functions.  Also to "splat" tuples into fixed-arity
+  functions.  
--- devel/scalaSyntax/splat.scala
+++ devel/scalaSyntax/splat.scala
@@ -2,63 +2,64 @@ package scalaSyntax
 
 object splat {
 
-  // For vatiadic functions, a splat type construct exists.
-
+  // For variadic functions, a splat type construct exists
+  // for variable length datastructures.
   def bar(xs: Double*) = xs.foldLeft(0: Double) {_ + _}
 
-  // For fixed-arity functions, concrete types
-
+  // For fixed-arity functions, we can accomplish splat 
+  // functionality for tuples:
   def foo(n: Int, x: Double, y: Double): Double = n*x + y
 
-  type Foo3T = (Int, Double, Double)
+  def fooC(n: Int)(x: Double)(y: Double): Double = n*x + y
 
-  /** Example of tupling.
-   *
-   *  Turning a 3 argument function into a
-   *  single argument function of a 3-tuple.
-   *
-   *  Unsugared:     (foo(_,_,_)).tupled
-   *
-   *  Full sacarine: import scala.language.postfixOps
-   *                 foo _ tupled
-   *
-   *    when fully applied, import not needed
-   *    example: val p = foo _ tupled (2, 3.0, 2.5)
-   *     
-   */
-  def fooTupled: Foo3T => Double = (foo _).tupled
+  type Tup3 = (Int, Double, Double)
+
+  def foo_T: Tup3 => Double = (foo _).tupled
+
+  def fooC_UT: Tup3 => Double = 
+    Function.uncurried(fooC _).tupled
 
   def main(args: Array[String]) = {
 
-    val xs: Array[Double] = Array(1,2,3,4,5,6,7,8,9,10)
-    val ys: List[Double]  =  List(1,2,3,4,5,6,7,8,9,10)
+    val xs: Array[Double] = Array(1,2,3,4,5)
+    val ys: List[Double]  =  List(1,2,3,4,5)
+    val ss: Stream[Double] = ys.toStream
 
     print("xs = "); println(xs.toString)  // Java objects are brain dead.
                                           // Who but the compiler cares
                                           // what the bloody reference is?
-    println("xs = Array(1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0)")
+    println("xs = Array(1.0, 2.0, 3.0, 4.0, 5.0)")
     print("ys = "); println(ys)
+    print("ss = "); println(ss)
 
-    println("Test variadic function:")
+    println("\nTest variadic function:")
 
-    print("\nbar(1,2,3,4,5,6,7,8,9,10) = ")
-    println(bar(1,2,3,4,5,6,7,8,9,10))
+    print("bar(1,2,3,4,5) = "); println(bar(1,2,3,4,5))
     print("bar(xs: _*) = "); println(bar(xs: _*))
     print("bar(ys: _*) = "); println(bar(ys: _*))
+    print("bar(ss: _*) = "); println(bar(ss: _*))
 
     println("\nTest fixed-arity function:")
 
     print("foo(2, 3.0, 2.5) = ")
     println(foo(2, 3.0, 2.5))
 
-    print("fooTupled((2, 3.0, 2.5)) = ")
-    println(fooTupled((2, 3.0, 2.5)))
+    print("foo_T((2, 3.0, 2.5)) = ")
+    println(foo_T((2, 3.0, 2.5)))
 
     print("foo _ tupled (2, 3.0, 2.5) = ")
     println(foo _ tupled (2, 3.0, 2.5))
 
-    print("(foo(_,_,_)).tupled((2, 3.0, 2.5)) = ")
-    println((foo(_,_,_)).tupled((2, 3.0, 2.5)))
+    println("\nTest curried fixed-arity function:")
+
+    print("fooC(2)(3.0)(2.5) = ")
+    println(fooC(2)(3.0)(2.5))
+
+    print("fooC_UT((2, 3.0, 2.5)) = ")
+    println(fooC_UT((2, 3.0, 2.5)))
+
+    print("Function.uncurried(fooC _) tupled (2, 3.0, 2.5) = ")
+    println(Function.uncurried(fooC _) tupled (2, 3.0, 2.5))
 
   }
 

10ee89150b8c65e80d5980894150e5d2516f7a72
Geoffrey Scheller <geoffrey@scheller.com>
2018-03-05 11:42:02 -0700

    Added some print statements since script not interactive.

--- grok/grokPython/deepLearning/mnistKeras.py
+++ grok/grokPython/deepLearning/mnistKeras.py
@@ -9,31 +9,34 @@ from keras.datasets import mnist
 (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
 
 # Training Data:
-train_images.shape
-len(train_labels)
-train_labels
+print('\ntrain_images.shape =', train_images.shape)
+print('len(train_labels) =', len(train_labels))
+print('train_labels =', train_labels)
 
 # Test Data:
-test_images.shape
-len(test_labels)
-test_labels
+print('\ntest_images.shape =', test_images.shape)
+print('len(test_labels) =', len(test_labels))
+print('test_labels =', test_labels)
 
 ### Build the network
 
 from keras import models
 from keras import layers
 
+print('\nBuild the network')
 network = models.Sequential()
 network.add(layers.Dense(512, activation='relu', input_shape=(28 * 28,)))
 network.add(layers.Dense(10, activation='softmax'))
 
 # Add loss function
+print('\nAdd loss function.')
 network.compile(optimizer='rmsprop',
                 loss='categorical_crossentropy',
                 metrics=['accuracy'])
 
 ### Reshape/format data to match network.
 
+print('\nReshape the network.')
 train_images = train_images.reshape((60000, 28 * 28))
 train_images = train_images.astype('float32') / 255
 
@@ -44,15 +47,18 @@ test_images = test_images.astype('float32') / 255
 
 from keras.utils import to_categorical
 
+print('\nCategorically encode the labels.')
 train_labels = to_categorical(train_labels)
 test_labels = to_categorical(test_labels)
 
 ### Train the network.
 
+print('\nTrain the network.')
 network.fit(train_images, train_labels, epochs=5, batch_size=128)
 
 ### Run network with training data.
 
+print('\nRun network with training data.')
 test_loss, test_acc = network.evaluate(test_images, test_labels)
 print('\ntest_acc:', test_acc)
 

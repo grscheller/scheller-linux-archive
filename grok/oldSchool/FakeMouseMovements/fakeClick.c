f2ba7f27ddd9dcbb3fa4f9d32f5624dec597979b
grscheller <geoffrey@scheller.com>
2018-11-25 14:14:02 -0700

    Updated grok/oldSchool/FakeMouseMovements
     - cleaned up C code
     - introduced some C++-isms (compiling "gcc -std=c99")
     - minimal argument error checking
     - added comments for X-windows stuff
     - no changes to X-Windows functionality
     - noticed some redundancy in X-Windows code
     - compiler "gcc -Wall" complains usleep function is depricated

--- grok/oldSchool/FakeMouseMovements/fakeClick.c
+++ grok/oldSchool/FakeMouseMovements/fakeClick.c
@@ -1,7 +1,11 @@
 /*
  *   Fake a mouse click at position x,y
  *
- *     Usage fakeClick 1000 100
+ *     Usage: fakeClick 1000 100
+ *
+ *   Positions cursor x units down & y units to right from 
+ *   the upper left hand courner of screen.  Then performs
+ *   a left mouse button click.
  *
  *   Got original code from Stackoverflow:
  *     https://stackoverflow.com/questions/20595716/control-mouse-by-writing-to-dev-input-mice/20772019#20772019
@@ -24,7 +28,7 @@ void mouseClick(int button) {
 
     if(display == NULL)
     {
-        fprintf(stderr, "Error in opening display!\n");
+        fprintf(stderr, "Error in opening display!!!\n");
         exit(EXIT_FAILURE);
     }
 
@@ -34,17 +38,34 @@ void mouseClick(int button) {
     event.xbutton.button = button;
     event.xbutton.same_screen = True;
 
-    XQueryPointer(display, RootWindow(display, DefaultScreen(display)), &event.xbutton.root, &event.xbutton.window, &event.xbutton.x_root, &event.xbutton.y_root, &event.xbutton.x, &event.xbutton.y, &event.xbutton.state);
-
+    XQueryPointer(display,
+                  RootWindow(display,
+                  DefaultScreen(display)),
+                  &event.xbutton.root,
+                  &event.xbutton.window,
+                  &event.xbutton.x_root,
+                  &event.xbutton.y_root,
+                  &event.xbutton.x,
+                  &event.xbutton.y,
+                  &event.xbutton.state);
     event.xbutton.subwindow = event.xbutton.window;
 
     while(event.xbutton.subwindow) {
         event.xbutton.window = event.xbutton.subwindow;
-
-        XQueryPointer(display, event.xbutton.window, &event.xbutton.root, &event.xbutton.subwindow, &event.xbutton.x_root, &event.xbutton.y_root, &event.xbutton.x, &event.xbutton.y, &event.xbutton.state);
+        XQueryPointer(display,
+                      event.xbutton.window, 
+                      &event.xbutton.root,
+                      &event.xbutton.subwindow,
+                      &event.xbutton.x_root,
+                      &event.xbutton.y_root,
+                      &event.xbutton.x,
+                      &event.xbutton.y,
+                      &event.xbutton.state);
     }
 
-    if(XSendEvent(display, PointerWindow, True, 0xfff, &event) == 0) fprintf(stderr, "Error\n");
+    if (XSendEvent(display, PointerWindow, True, 0xfff, &event) == 0) {
+       fprintf(stderr, "Error\n");
+    }
 
     XFlush(display);
 
@@ -53,25 +74,46 @@ void mouseClick(int button) {
     event.type = ButtonRelease;
     event.xbutton.state = 0x100;
 
-    if(XSendEvent(display, PointerWindow, True, 0xfff, &event) == 0) fprintf(stderr, "Error\n");
+    if (XSendEvent(display, PointerWindow, True, 0xfff, &event) == 0) {
+       fprintf(stderr, "Error\n");
+    }
 
     XFlush(display);
-
     XCloseDisplay(display);
 }
 
-int main(int argc,char * argv[]) {
-   int x, y;
-   x=atoi(argv[1]);
-   y=atoi(argv[2]);
-   Display *display = XOpenDisplay(0);
-   Window root = DefaultRootWindow(display);
+void usage_on_failure() {
+    fprintf(stderr, "ERROR: Invalid arguments!!!\n");
+    fprintf(stderr, "  Usage: fakeClick x_pos y_pos\n");
+    fprintf(stderr, "         where x_pos & y_pos both >= 0\n");
 
-   XWarpPointer(display, None, root, 0, 0, 0, 0, x, y);
+    exit(EXIT_FAILURE);
+}
 
-   mouseClick(Button1);
-   XFlush(display);
+int main(int argc, char * argv[]) {
 
-   XCloseDisplay(display);
-   return 0;
+    // Parse arguments
+    if ( argc != 3 ) usage_on_failure();
+
+    int x = atoi(argv[1]);
+    int y = atoi(argv[2]);
+
+    if ( x <= 0 || y <= 0 ) usage_on_failure();
+
+    Display *display = XOpenDisplay(0);
+    Window root = DefaultRootWindow(display);
+
+    // Move mouse pointer to default position x,y 
+    // on the default root window or the display.
+    XWarpPointer(display, None, root, 0, 0, 0, 0, x, y);
+
+    // Fake left mouse button single click
+    mouseClick(Button1);  // Where does Button1 come from?
+
+    // Clean up
+    XFlush(display);
+    XCloseDisplay(display);
+
+    return 0;
+
 }

7b1f9c9c5097da962ef53fdd5f9e85de9a3e3b34
grscheller <geoffrey@scheller.com>
2018-12-05 20:19:49 -0700

    Coding convension change grok/oldSchool/SystemsProgramming
     - Moved declarations from top of programs
     - In line with modern conventions of defining
       variable before first use
     - Only file changed was myls.c

--- grok/oldSchool/SystemsProgramming/myls.c
+++ grok/oldSchool/SystemsProgramming/myls.c
@@ -10,19 +10,17 @@
 int
 main(int argc, char *argv[])
 {
-    DIR            *dp;
-    struct dirent  *dirp;
-
     if (argc != 2)
         err_quit("usage: myls <path-to-directory>");
 
+    DIR *dp;
     if ( (dp = opendir(argv[1])) == NULL )
         err_sys("myls: Can't open %s", argv[1]);
 
+    struct dirent  *dirp;
     while ( (dirp = readdir(dp)) != NULL )
         printf("%s\n", dirp->d_name);
 
     closedir(dp);
     exit(0);
-
 }

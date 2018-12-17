7125c46a7f0309794901f75412c66c7b5e1db4cb
grscheller <geoffrey@scheller.com>
2018-12-16 20:52:05 -0700

    Updated grok/grokC_C++
     - Made C++InfinityAndBeyond/hw.cpp multithreaded

--- grok/grokC_C++/C++InfinityAndBeyond/hw.cpp
+++ grok/grokC_C++/C++InfinityAndBeyond/hw.cpp
@@ -1,23 +1,45 @@
 /*
  *  Traditional K&R Hello World program done in C++.
- *
- *  Illustrates:
- *
- *  1. Type infarence
- *  2. IOSTREAM library
- *  3. Still lets you "fiddle the bits" with
- *     pointers to chracter arrays
- *  4. Use of namespaces
- *  5. Unlike ANSI C, main's prototype indicates
- *     that it takes no arguments.
  */
 #include <iostream>
+#include <thread>
 
-int main() {
+auto hw = "  Hello World!";
+auto gw = "Goodbye World!";
 
-    auto hw = "  Hello World!";
-    std::cout << hw+2 << "\n";
+void hello()
+{
+    using namespace std;
+
+    cout << hw+2 << endl;
+}
+
+int main()
+{
+    std::thread t(hello);
+    t.join();
+    std::cout << gw << '\n';
 
     return 0;
-
 }
+
+/*
+ *  Notes:
+ *
+ *  1. Type infarence via auto keyword
+ *  2. IOSTREAM library
+ *  3. Use of namespaces
+ *  4. Still lets you "fiddle the bits" with
+ *     pointers to chracter arrays
+ *  5. Unlike ANSI C, main's prototype indicates
+ *     that it takes no arguments.
+ *  6. Function overloading makes << easy to deal with
+ *  7. Built in std::thread library even with base compile
+ *  8. Currently not specifying any sort of "standard"
+ *  8. Need to compile with
+ *       g++ -Wall -pthread hw.cpp -o hw
+ *     otherwise linker error with pthread_create
+ *     undefined.  Got the -pthread from the pthread_create 
+ *     Linux, not POSIX, manpage.
+ */
+

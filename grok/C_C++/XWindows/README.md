2b3f55ea09d93ad8e1d4e215a2d8e2b13732f7a1
grscheller <geoffrey@scheller.com>
2019-01-12 12:28:50 -0500

    Updated grok/C_C++/XWindows/jiggleMouse.c
     - jiggleMouse.c final form
     - Added non-trivial content to XWindows/README.md

--- grok/C_C++/XWindows/README.md
+++ grok/C_C++/XWindows/README.md
@@ -1,3 +1,14 @@
 #  X Windows Programming
-Examples of low level XWindows coding.  Currently written in xlib,
-debating redoing in xcb.
+Examples of low level XWindows coding.
+
+### XLIB Examples:
+#### jiggleMouse.c
+* Periodically simulates a small mouse movement - a "jiggle"
+* Default parameters not noticeable by user
+* Keeps my X session at work from locking for "lack of user activity"
+#### fakeClick.c
+* Fake a mouse click at a given position
+* Program still needs debugging
+* Mouse moves but "click" not registering with X-GUI applications
+
+### XCB Examples:
--- grok/C_C++/XWindows/jiggleMouse.c
+++ grok/C_C++/XWindows/jiggleMouse.c
@@ -1,38 +1,46 @@
 /*
- *   Jiggle mouse movement to keep system alive
+ *   Simulate mouse movements to keep system alive
  *
- *     Usage: jiggleMouse
+ *     Usage:  jiggleMouse [delay [timeout [jiggle]]]
  *
  */
 #include <X11/Xlib.h>
-#include <assert.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <malloc.h>
+#include <stdbool.h>
 
-int main(int argc, char * argv[]) {
-
-    int jiggle;  // amount of X-coords to "jiggle the mouse"
-    int delay;   // seconds between jiggles
-
+int
+main(int argc, char * argv[])
+{
     /* Parse cmdline arguments */
-    if (argc > 3) {
+    int jiggle = 1;   // amount of X-coords to "jiggle the mouse"
+    int delay = 300;  // seconds between jiggles (5 minutes)
+    int timeout = 0;  // no timeout
+    bool countdown;
+
+    if (argc > 4) {
         fprintf(stderr, "Error: invalid number of arguments\n");
-        fprintf(stderr, "  Usage: jiggleMouse [delay [jiggle]]\n");
+        fprintf(stderr, "  Usage: jiggleMouse [delay [timeout [jiggle]]]\n");
         fprintf(stderr, "    delay  = time in seconds between jiggles\n");
         fprintf(stderr, "    jiggle = amount to jiggle\n");
+        fprintf(stderr, "    timeout = end after timeout seconds\n");
         exit(EXIT_FAILURE);
+    } else if (argc == 4) {
+        delay = atoi(argv[1]);
+        timeout = atoi(argv[2]);
+        jiggle = atoi(argv[3]);
     } else if (argc == 3) {
         delay = atoi(argv[1]);
-        jiggle = atoi(argv[2]);
+        timeout = atoi(argv[2]);
     } else if (argc == 2) {
         delay = atoi(argv[1]);
-        jiggle = 1;
-    } else {
-        delay = 300;
-        jiggle = 1;
     }
+
+    if (timeout == 0)
+        countdown = False;
+    else
+        countdown = True;
 
     /* Get display or fail */
     Display *display = XOpenDisplay(NULL);
@@ -55,9 +63,9 @@ int main(int argc, char * argv[]) {
     int root_x, root_y, win_x, win_y;
     unsigned int mask_return;
 
-    while(1) {
-        /* Find the mouse and error out if unsucessful */
-        Bool mouse_found;
+    while(timeout >= 0) {
+        /* Find current mouse position and error out if unsucessful */
+        bool mouse_found;
         for (ii = 0; ii < number_of_screens; ii++) {
             mouse_found = XQueryPointer(display,
                           root_windows[ii],
@@ -84,8 +92,10 @@ int main(int argc, char * argv[]) {
                      0, 0, 0, 0, root_x, root_y);
         XFlush(display);
 
-        /* Sleep until next jiggle */
+        /* Sleep and setup for next jiggle */
         sleep(delay);
+        if (countdown)
+            timeout -= delay;
     }
 
     free(root_windows);

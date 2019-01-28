de3f03535403119474a17f3c71efb725e74a82ae
Geoffrey Scheller <geoffrey@scheller.com>
2019-01-28 12:52:19 -0700

    APUE now can target Cygwin
     - config.mk defaults to Linux
     - my test system are
       - Arch Linux (rolling release)
       - CentOS 7 Linux
       - Cygwin on a Windows 10 (64-bit)

--- grok/C/APUE/src/intro/intro.mk
+++ grok/C/APUE/src/intro/intro.mk
@@ -3,11 +3,11 @@
 PATH_INTRO := src/intro
 PROGS_INTRO := buffered_cat pidInfo simpleLs tinyShell unbuffered_cat
 
-PROGS_INTRO_FULL := $(addprefix $(PATH_INTRO)/,$(PROGS_INTRO))
+PROGS_INTRO_FULL := $(addprefix $(PATH_INTRO)/,$(addsuffix $(EXT),$(PROGS_INTRO)))
 
 intro: $(PROGS_INTRO_FULL)
 
-$(PATH_INTRO)/%: $(PATH_INTRO)/%.c $(APUE_H) $(LIBAPUE_A)
+$(PATH_INTRO)/%$(EXT): $(PATH_INTRO)/%.c $(APUE_H) $(LIBAPUE_A)
 	$(CC) $(CFLAGS) $(CPPFLAGS) -o $@ $< $(LDFLAGS)
 
 cleanintro:

3ee2b882fde397c2c65de0812d30a59a44ca0043
grscheller <geoffrey@scheller.com>
2019-04-16 14:55:15 -0600

    ToInfinityAndBeyond/hw.cpp change
     - Preparing to work more on C++/ToInfinityAndBeyond
     - Working with Stroustrup's "A Tour of C++"
     - Using C++ Universal Initialization Feature
       - Turns off automatic built in type conversions

--- grok/C++/ToInfinityAndBeyond/hw.cpp
+++ grok/C++/ToInfinityAndBeyond/hw.cpp
@@ -4,8 +4,8 @@
 #include <iostream>
 #include <thread>
 
-auto hw = "  Hello World!";
-auto gw = "Goodbye World!";
+auto hw {"  Hello World!"};
+auto gw {"Goodbye World!"};
 
 void hello()
 {
@@ -28,15 +28,17 @@ int main()
  *
  *  1. Type infarence via auto keyword
  *  2. IOSTREAM library
- *  3. Use of namespaces
- *  4. Still lets you "fiddle the bits" with
+ *  3. Uniform Initialization turns off Fortran-like automatic
+ *     data converion.
+ *  4. Use of namespaces
+ *  5. Still lets you "fiddle the bits" with
  *     pointers to chracter arrays
- *  5. Unlike ANSI C, main's prototype indicates
+ *  6. Unlike ANSI C, main's prototype indicates
  *     that it takes no arguments.
- *  6. Function overloading makes << easy to deal with
- *  7. Built in std::thread library even with base compile
- *  8. Currently not specifying any sort of "standard"
- *  8. Need to compile with
+ *  7. Function overloading makes << easy to deal with
+ *  8. Built in std::thread library even with base compile
+ *  9. Currently not specifying any sort of "standard"
+ * 10. Need to compile with
  *       g++ -Wall -pthread hw.cpp -o hw
  *     otherwise linker error with pthread_create
  *     undefined.  Got the -pthread from the pthread_create 

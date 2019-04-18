0e3d52f5ee61df137b8523903d68fc9f0ae73074
grscheller <geoffrey@scheller.com>
2019-04-18 14:47:58 -0600

    Got curiously recursive template partern working
     - C++/ToInfinityAndBeyond/crtp.cpp

--- grok/C++/ToInfinityAndBeyond/crtp.cpp
+++ grok/C++/ToInfinityAndBeyond/crtp.cpp
@@ -3,6 +3,8 @@
  */
 #include <iostream>
 
+using namespace std;
+
 template <typename T>
 struct counter
 {
@@ -21,6 +23,14 @@ struct counter
         ++objects_alive;
     }
 
+    int getCreated() {
+        return objects_created;
+    }
+
+    int getAlive() {
+        return objects_alive;
+    }
+
   protected:
     ~counter()
     {
@@ -30,15 +40,25 @@ struct counter
 template <typename T> int counter<T>::objects_created{0};
 template <typename T> int counter<T>::objects_alive{0};
 
-class Dog : counter<Dog>
+class Dog : public counter<Dog>
 {
 
 };
 
-class Robot : counter<Robot>
+class Robot : public counter<Robot>
 {
 
 };
+
+void callWalter()
+{
+    Dog walter;
+
+    cout << "Entered callWalter\n";
+    cout << "Number of Dogs created: " << walter.getCreated() << endl;
+    cout << "Number of Dogs alive: "   << walter.getAlive()   << endl;
+    cout << "Leaving callWalter\n";
+}
 
 int main()
 {
@@ -46,17 +66,16 @@ int main()
     Dog flash;
     Robot robbie;
 
-    using namespace std;
+    cout << "Number of Robots created: " << robbie.getCreated() << endl;
+    cout << "Number of Robots alive: "   << robbie.getAlive()   << endl;
+    cout << "Number of Dogs created: " << flash.getCreated() << endl;
+    cout << "Number of Dogs alive: "   << fido.getAlive()  << endl;
 
-    // cout << "Number of Dogs created: " << Dog.objects_created << endl;
-    // cout << "Number of Dogs alive: "   << fido.objects_alive   << endl;
+    callWalter();
+
+    cout << "Number of Dogs created: " << fido.getCreated() << endl;
+    cout << "Number of Dogs alive: "   << flash.getAlive()  << endl;
 
     return 0;
 }
-
-/*
- *  Notes:
- *
- *  1. 
- */
 

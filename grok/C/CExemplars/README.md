83d8e82d37b40e451eae366027c3185b578907c6
grscheller <geoffrey@scheller.com>
2020-10-20 23:10:26 -0400

    Added grok/C/CExemplars/makeMessage.c
     - Wonderful exemplar from the Linux Manpage project
     - create variable lenth formatted messages
     - allocate sufficient space for them

--- grok/C/CExemplars/README.md
+++ grok/C/CExemplars/README.md
@@ -2,3 +2,4 @@
 Best practices for ANSI-C coding/algorithms/compilation.
 * __removeSpaces.c__: O(n) string manipulation
 * __sumit.c__: [Compiler over-optimization](sumit.md)
+* __makeMessage.c__: allocate space & create a formatted message
---
+++ grok/C/CExemplars/makeMessage.c
@@ -0,0 +1,69 @@
+/*
+ * Allocate the necessary space and create a formatted string.
+ *
+ * Example taken from release 5.08 of the Linux manpages for printf.
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+char *make_message(const char *fmt, ...)
+{
+    int n = 0;
+    size_t size = 0;
+    char *p = NULL;
+    va_list ap;
+
+    /* Determine required size */
+
+    va_start(ap, fmt);
+    n = vsnprintf(p, size, fmt, ap);
+    va_end(ap);
+
+    if (n < 0)
+        return NULL;
+
+    /* One extra byte for '\0' */
+
+    size = (size_t) n + 1;
+    p = malloc(size);
+    if (p == NULL)
+        return NULL;
+
+    va_start(ap, fmt);
+    n = vsnprintf(p, size, fmt, ap);
+    va_end(ap);
+
+    if (n < 0) {
+        free(p);
+        return NULL;
+    }
+
+    return p;
+}
+
+int main(int argc, char *argv[])
+{
+    char *myMessage = NULL;
+    char *name;
+
+    if (argc > 1)
+        name = argv[1];
+    else
+        name = "Geoffrey Scheller";
+
+    char *formatStr = "Hello there %s, %s and %s,\nhow are you all today?\n";
+
+    myMessage = make_message(formatStr, "Joe Blow", "Jane Doe", name);
+
+    if (myMessage != NULL)
+        printf("%s", myMessage);
+    else {
+        printf("Something went wrong.\n");
+        return 1;
+    }
+
+    return 0;
+}

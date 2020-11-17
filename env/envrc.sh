e8af03b12b2099c7efb6162b2cb71c3c5f7fef1e
grscheller <geoffrey@scheller.com>
2020-11-17 06:11:02 -0500

    Personnel environment configuration in env/
     - Using urxvt as my default terminal
       - A lot of configuration to get working nicely
       - created ~/.Xresource file to configure it
       - adjusted tm() in .bashrc
     - urxvt only does Primary X buffer
       - configured gpaste to share between Desktop and Primary buffers
       - vim can interact with both through "+ and "*
     - Double quoted "$PATH" in .envrc to protect whitespace better

--- env/envrc.sh
+++ env/envrc.sh
@@ -49,29 +49,34 @@ export PIP_REQUIRE_VIRTUALENV=true
 [ -z "$VIRGIN_PATH" ] && export VIRGIN_PATH="$PATH"
 
 ## Chocolatey for Windows when in MSYS2 or CYGWIN
-PATH=$PATH:/cygdrive/c/ProgramData/chocolatey/bin
-PATH=$PATH:/c/ProgramData/chocolatey/bin
+PATH="$PATH":/cygdrive/c/ProgramData/chocolatey/bin
+PATH="$PATH":/c/ProgramData/chocolatey/bin
 
 # For Termux environment on my Android cell phone
-PATH=$PATH:/data/data/com.termux/files/usr/bin/applets
+PATH="$PATH":/data/data/com.termux/files/usr/bin/applets
 
 # Put home bin directory near end
-PATH=$PATH:~/bin
+PATH="$PATH":~/bin
 
 # Put a relative bin directory at end of PATH, this is for
 # projects where the user takes up residence in the project's
 # root directory.
-PATH=$PATH:bin 
+PATH="$PATH":bin 
+
+# Rudy tool chain
+#   mostly for Markdown linter ~/.gem/ruby/2.7.0/bin/mdl
+#   to install: $ gem install mdl
+PATH=~/.gem/ruby/2.7.0/bin:"$PATH"
 
 # For Haskell locally contained and administered via stack
-PATH=~/.local/bin:$PATH
+PATH=~/.local/bin:"$PATH"
 
 # Location Rust Toolchain
-PATH=~/.cargo/bin:$PATH
+PATH=~/.cargo/bin:"$PATH"
 
 # Locally installed SBT (Scala Build Tool)
-PATH=~/opt/sbt/bin:$PATH
-PATH=/c/Program\ Files\ \(x86\)/sbt/bin:$PATH
+PATH=~/opt/sbt/bin:"$PATH"
+PATH=/c/Program\ Files\ \(x86\)/sbt/bin:"$PATH"
 # Path to Java on Windows 10
 PATH=/c/Program\ Files\ \(x86\)/Common\ Files/Oracle/Java/javapath:"$PATH"
 

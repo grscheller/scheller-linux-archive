1ffecd769c4709cb7f5c7e71c3b12cd295b5187c
grscheller <geoffrey@scheller.com>
2021-10-18 11:06:58 -0600

    Updated C++/ToInfinityAndBeyond/crtp/counter.cpp
     - trying out Neovim configuration from dotfiles repo
       - added a method to Dog Class to make classes different
       - reformatted entire file counter.cpp via ",F" keybinding
         - surprised to see 3 space indentation
         - expected 4, hoping not to get 8
     - unrelated web/homepage.html update for work

--- grok/C++/ToInfinityAndBeyond/crtp/counter.cpp
+++ grok/C++/ToInfinityAndBeyond/crtp/counter.cpp
@@ -5,87 +5,71 @@
 
 using namespace std;
 
-template <typename T>
-struct counter
-{
-    static int objects_created;
-    static int objects_alive;
+template <typename T> struct counter {
+  static int objects_created;
+  static int objects_alive;
 
-    counter()
-    {
-        ++objects_created;
-        ++objects_alive;
-    }
+  counter() {
+    ++objects_created;
+    ++objects_alive;
+  }
 
-    counter(const counter&)
-    {
-        ++objects_created;
-        ++objects_alive;
-    }
+  counter(const counter &) {
+    ++objects_created;
+    ++objects_alive;
+  }
 
-    int getCreated() {
-        return objects_created;
-    }
+  int getCreated() { return objects_created; }
 
-    int getAlive() {
-        return objects_alive;
-    }
+  int getAlive() { return objects_alive; }
 
-  protected:
-    ~counter()
-    {
-        --objects_alive;
-    }
+protected:
+  ~counter() { --objects_alive; }
 };
 template <typename T> int counter<T>::objects_created{0};
 template <typename T> int counter<T>::objects_alive{0};
 
-class Dog : public counter<Dog>
-{
-
+class Dog : public counter<Dog> {
+public:
+  void speak() { cout << "Bark, Bark" << endl; }
 };
 
-class Robot : public counter<Robot>
-{
+class Robot : public counter<Robot> {};
 
-};
+class Goat : public counter<Goat> {};
 
-class Goat : public counter<Goat>
-{
+void callWalter() {
+  Dog walter;
 
-};
-
-void callWalter()
-{
-    Dog walter;
-
-    cout << "Entered callWalter\n";
-    cout << "Number of Dogs created: " << walter.getCreated() << endl;
-    cout << "Number of Dogs alive: "   << walter.getAlive()   << endl;
-    cout << "Leaving callWalter\n";
+  cout << "Entered callWalter\n";
+  cout << "Number of Dogs created: " << walter.getCreated() << endl;
+  cout << "Number of Dogs alive: " << walter.getAlive() << endl;
+  cout << "Leaving callWalter\n";
 }
 
-int main()
-{
-    Dog fido;
-    Dog flash;
-    Robot robbie;
-    Goat nanny;
-    Goat billy;
+int main() {
+  Dog fido;
+  Dog flash;
+  Robot robbie;
+  Goat nanny;
+  Goat billy;
 
-    cout << "Number of Robots created: " << robbie.getCreated() << endl;
-    cout << "Number of Robots alive: "   << robbie.getAlive()   << endl;
-    cout << "Number of Dogs created: " << flash.getCreated() << endl;
-    cout << "Number of Dogs alive: "   << fido.getAlive()  << endl;
+  cout << "Number of Robots created: " << robbie.getCreated() << endl;
+  cout << "Number of Robots alive: " << robbie.getAlive() << endl;
+  cout << "Number of Dogs created: " << flash.getCreated() << endl;
+  cout << "Number of Dogs alive: " << fido.getAlive() << endl;
 
-    callWalter();
+  callWalter();
 
-    cout << "Number of Dogs created: " << fido.getCreated() << endl;
-    cout << "Number of Dogs alive: "   << flash.getAlive()  << endl;
+  cout << "Number of Dogs created: " << fido.getCreated() << endl;
+  cout << "Number of Dogs alive: " << flash.getAlive() << endl;
 
-    cout << "Nanny say there were " << nanny.getCreated() << " goats created." << endl;
-    cout << "Billy say there are "  << billy.getAlive() << " goats alive." << endl;
+  cout << "Nanny say there were " << nanny.getCreated() << " goats created."
+       << endl;
+  cout << "Billy say there are " << billy.getAlive() << " goats alive." << endl;
 
-    return 0;
+  cout << "Fido speaks!" << endl;
+  fido.speak();
+
+  return 0;
 }
-

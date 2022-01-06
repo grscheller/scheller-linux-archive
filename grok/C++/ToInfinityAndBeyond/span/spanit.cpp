5d7599a739cf56d83822958013ef51c2e617cdb7
grscheller <geoffrey@scheller.com>
2022-01-06 13:37:39 -0500

    Worked on ToInfinityAndBeyond/span
     - exploring use case of constexpr
       - constexpr introduced in C++11
       - specifies that value can be calculated at compile time
       - tested how it affects function calls

--- grok/C++/ToInfinityAndBeyond/span/spanit.cpp
+++ grok/C++/ToInfinityAndBeyond/span/spanit.cpp
@@ -5,21 +5,39 @@
 
 using namespace std;
 
-void f(span<int> s)
+void f(span<int> seq)
 {
-    cout << s.size() << ": ";
-    for (auto &x: s) cout << x << ' ';
+    cout << seq.size() << ": ";
+    for (auto &x: seq) cout << x << ' ';
+    cout << endl;
+}
+
+void g(const auto& seq)
+{
+    for (auto &x: seq) cout << x << ' ';
     cout << endl;
 }
 
 int main(void)
 {
-    int a[]{1, 2, 42};
-    vector<int> vec{3, 1, 4, 1, 5, 9};
-    array<int, 7> arr{30, 14, 57, 99, 100, -11, 5};
-    f(a);
+    int a[] {1, 2, 42, 314159};
+    constexpr int b[] {10, 9, 8, 8, 7, 6, 5, 4, 3*4 - 1};
+    vector<int> vec {3, 1, 4, 1, 5, 9};
+    array<int, 7> arr {30, 14, 57, 99, 100, -11, 5};
     f(vec);
     f(arr);
+    f(a);
+
+    auto bs = span(b);
+    cout << bs.size() << ": ";
+    for (auto &x: bs) cout << x << ' ';
+    cout << "\n\n";
+
+    g(vec);
+    g(arr);
+    g(a);
+    g(b);
+    cout << endl;
 
     return 0;
 }

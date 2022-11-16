c1cdbb04b63921a6b63bce0ca04d4651331dfd76
grscheller <geoffrey@scheller.com>
2022-11-15 18:23:32 -0700

    Reformatted grok/Haskell/pythag-triples
     - practicing using new Neovim LSP keybindings
       - see grscheller/dotfiles commit on 2022-11-15

--- grok/Haskell/pythag-triples/lib/PythagTriples.hs
+++ grok/Haskell/pythag-triples/lib/PythagTriples.hs
@@ -1,13 +1,13 @@
-module PythagTriples (Triple,
-                      printTriples,
-                      pythagTriplesOrdered1,
-                      pythagTriplesOrdered2,
-                      pythagTriplesFast,
-                      showTriple,
-                      sortTriple) where
+module PythagTriples ( Triple
+                     , printTriples
+                     , pythagTriplesOrdered1
+                     , pythagTriplesOrdered2
+                     , pythagTriplesFast
+                     , showTriple
+                     , sortTriple ) where
 
-import Data.List       (intercalate)
-import Data.List.Split (splitOn)
+import           Data.List       (intercalate)
+import           Data.List.Split (splitOn)
 
 type Triple = (Int, Int, Int)
 
@@ -25,17 +25,14 @@ printTriples = mapM_ (putStrLn . showTriple)
 --   a & c are always odd, b is always even
 pythagTriplesFast :: Int -> Int -> [Triple]
 pythagTriplesFast start end =
-  let n = end
-      m =
-        if start < 2
-          then 2
-          else start
-   in [ (a, b, c) | k <- [m .. n],
-          j <- [k - 1, k - 3 .. 1],
-          gcd j k == 1,
-          let a = k * k - j * j,
-          let b = 2 * j * k,
-          let c = k * k + j * j
+   let m = if start < 2 then 2 else start
+       n = end
+   in [ (a, b, c) | k <- [m .. n]
+      , j <- [k - 1, k - 3 .. 1]
+      , gcd j k == 1
+      , let a = k * k - j * j
+      , let b = 2 * j * k
+      , let c = k * k + j * j
       ]
 
 -- | Generate ordered Pythagorean Triples first by a then b.
@@ -43,35 +40,29 @@ pythagTriplesFast start end =
 --   before moving onto the next a.
 pythagTriplesOrdered1 :: Int -> Int -> [Triple]
 pythagTriplesOrdered1 start end =
-  let n = end
-      m =
-        if start < 3
-          then 3
-          else start
-   in [ (a, b, c) | a <- [m .. n],
-          b <- [a + 1, a + 3 .. ((a * a - 1) `div` 2)],
-          gcd b a == 1,
-          let csqr = a * a + b * b,
-          isPerfectSquare csqr,
-          let c = floorSqrt csqr
-      ]
+  let m = if start < 3 then 3 else start
+      n = end
+  in [ (a, b, c) | a <- [m .. n]
+     , b <- [a + 1, a + 3 .. ((a * a - 1) `div` 2)]
+     , gcd b a == 1
+     , let csqr = a * a + b * b
+     , isPerfectSquare csqr
+     , let c = floorSqrt csqr
+     ]
 
 -- | Generate ordered Pythagorean Triples first by b then a
 --   Will only find a's where a < b
 pythagTriplesOrdered2 :: Int -> Int -> [Triple]
 pythagTriplesOrdered2 start end =
-  let n = end
-      m =
-        if start < 4
-          then 4
-          else start
-   in [ (a, b, c) | b <- [m .. n],
-          a <- [(floorSqrt $ 2 * b + 1) .. b - 1],
-          gcd b a == 1,
-          let csqr = a * a + b * b,
-          isPerfectSquare csqr,
-          let c = floorSqrt csqr
-      ]
+  let m = if start < 4 then 4 else start
+      n = end
+  in [ (a, b, c) | b <- [m .. n]
+     , a <- [(floorSqrt $ 2 * b + 1) .. b - 1]
+     , gcd b a == 1
+     , let csqr = a * a + b * b
+     , isPerfectSquare csqr
+     , let c = floorSqrt csqr
+     ]
 
 -- Utility functions
 

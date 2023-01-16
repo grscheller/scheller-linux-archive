636703fd39edbe8ded71409a4db37548f36211bc
grscheller <geoffrey@scheller.com>
2023-01-16 16:13:14 -0500

    Updated grok/C/CUtils/realpath.c
     - based on Neovim LSP feedback
       - used with lazy.nvim

--- grok/C/CUtils/realpath.c
+++ grok/C/CUtils/realpath.c
@@ -3,15 +3,13 @@
 #include <limits.h>
 
 int
-main(int argc, char **argv)
-{
-  char rlpath[PATH_MAX];
-  if (argc==2)
-  {
-      if (realpath(argv[1], rlpath) == NULL)
-          exit(2);
-      fprintf(stdout, "%s\n", rlpath);
-      exit(0);
-  }
-  exit(1);
+main(int argc, char **argv) {
+    if (argc == 2) {
+        char rlpath[PATH_MAX];
+        if (realpath(argv[1], rlpath) == NULL)
+            exit(2);
+        fprintf(stdout, "%s\n", rlpath);
+        exit(0);
+    }
+    exit(1);
 }

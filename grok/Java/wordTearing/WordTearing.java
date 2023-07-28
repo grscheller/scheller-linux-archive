2ae22f30973751987c34afc4c51754f4c4d9fb1a
grscheller <geoffrey@scheller.com>
2023-07-28 15:45:30 -0600

    Updated grok/Java/wordTearing/WordTearing.java
    Caught some types while testing Scala Metals.

--- grok/Java/wordTearing/WordTearing.java
+++ grok/Java/wordTearing/WordTearing.java
@@ -3,12 +3,12 @@
  *  JVM can update adjacent elements of a byte array.
  *
  *  Ideally, separately executing threads, not interacting
- *  with each other's values, should not need synchronization
- *  to ensure sequential consistency.  Unfornuately, if the
+ *  with each other, should not need synchronization
+ *  to ensure sequential consistency. Unfortunately, if the
  *  underlying processor can only interact on a word basis,
  *  synchronization will be needed.
  *
- *  Most of this code is lifted verbatum from the Java
+ *  Most of this code is lifted verbatim from the Java
  *  language Specification, Java SE 8 Edition.
  */
 public class WordTearing extends Thread {
@@ -42,7 +42,7 @@ public class WordTearing extends Thread {
   }
 
   /**
-   * Eight Threads working indepently on each byte of a word.
+   * Eight Threads working independently on each byte of a word.
    *
    * The threads should not get in each others way unless
    * the underlying java implementation can only work on 

e23e7b7ee89d6bab083e50c916ac48a8851cc7a2
grscheller <geoffrey@scheller.com>
2023-08-22 14:22:24 -0600

    Updated grok/Zig/hw/hw.zig

--- grok/Zig/hw/hw.zig
+++ grok/Zig/hw/hw.zig
@@ -1,5 +1,12 @@
+//! The customary hello world first program. This is a top-level comment.
+//! I don't know if a program is considered a moduule or not.
 const std = @import("std");
 
-pub fn main() void {
-    std.debug.print("Hello, {s}!!!\n", .{"World"});
+pub fn main() !void {
+    // comments use 2 slashes, doc comments 3
+    const stdout = std.io.getStdOut().writer();
+
+    // A friendly greeting - zig does not like me making it a doc comment
+    const hello = "Hello";
+    try stdout.print("{s}, {s}!!!\n", .{ hello, "World" });
 }

10fc3ee5ebeb166991bb8b8adebafaf1d9c4c3e5
grscheller <geoffrey@scheller.com>
2024-04-18 07:37:07 -0600

    Finally more work on PythonForAI

--- courses/Udacity/PythonForAI/03-gaussian/gaussian.py
+++ courses/Udacity/PythonForAI/03-gaussian/gaussian.py
@@ -36,6 +36,7 @@ class Gaussian():
         data_list (list of floats) a list of floats extracted from the data file
     """
     def __init__(self, mu=0, sigma=1):
+        self.c = 1.0 / math.sqrt(2*math.pi)
         self.mean = mu
         self.stdev = sigma
         self.data = []
@@ -66,15 +67,6 @@ class Gaussian():
         Returns:
             float: standard deviation of the data set
         """
-
-        # TODO:
-        #   Calculate the standard deviation of the data set
-        #
-        #   The sample variable determines if the data set contains a sample or a population
-        #   If sample = True, this means the data is a sample.
-        #   Keep the value of sample in mind for calculating the standard deviation
-        #
-        #   Make sure to update self.stdev and return the standard deviation as well
 
         sigma = self.stdev
         n = len(self.data)
@@ -129,11 +121,13 @@ class Gaussian():
             None
         """
 
-        # TODO: Plot a histogram of the data_list using the matplotlib package.
-        #       Be sure to label the x and y axes and also give the chart a title
-        pass
-
-
+        # make the plot
+        fig, axis = plt.subplots()
+        axis.hist(self.data)
+        axis.set_title('Histogram of Data')
+        axis.set_ylabel('Data')
+        axis.set_ylabel('Count')
+        plt.show()
 
     def pdf(self, x):
         """Probability density function calculator for the gaussian distribution.
@@ -146,9 +140,14 @@ class Gaussian():
             float: probability density function output
         """
 
-        # TODO: Calculate the probability density function of the Gaussian distribution
-        #       at the value x. You'll need to use self.stdev and self.mean to do the calculation
-        pass
+        exp = math.exp
+        sqrt = math.sqrt
+        c = self.c
+
+        mu = self.mean
+        sigma = self.stdev
+
+        return (c/sigma)*exp(-0.5*((x - mu)/sigma)**2)
 
     def plot_histogram_pdf(self, n_spaces = 50):
 
@@ -164,25 +163,20 @@ class Gaussian():
 
         """
 
-        #TODO: Nothing to do for this method. Try it out and see how it works.
+        min_xs = min(self.data)
+        max_xs = max(self.data)
 
-        mu = self.mean
-        sigma = self.stdev
+        # calculates the interval between x values
+        interval = 1.0 * (max_xs - min_xs) / n_spaces
 
-        min_range = min(self.data)
-        max_range = max(self.data)
-
-         # calculates the interval between x values
-        interval = 1.0 * (max_range - min_range) / n_spaces
-
-        x = []
-        y = []
+        xs = []
+        ys = []
 
         # calculate the x values to visualize
         for i in range(n_spaces):
-            tmp = min_range + interval*i
-            x.append(tmp)
-            y.append(self.pdf(tmp))
+            x = min_xs + interval*i
+            xs.append(x)
+            ys.append(self.pdf(x))
 
         # make the plots
         fig, axes = plt.subplots(2,sharex=True)
@@ -191,9 +185,9 @@ class Gaussian():
         axes[0].set_title('Normed Histogram of Data')
         axes[0].set_ylabel('Density')
 
-        axes[1].plot(x, y)
+        axes[1].plot(xs, ys)
         axes[1].set_title('Normal Distribution for \n Sample Mean and Sample Standard Deviation')
         axes[0].set_ylabel('Density')
         plt.show()
 
-        return x, y
+        return xs, ys
---
+++ courses/Udacity/PythonForAI/03-gaussian/gaussian_test.py
@@ -0,0 +1,26 @@
+# Using pdoc3 for testing
+
+from gaussian import Gaussian
+
+gauss = Gaussian(25, 2)
+euler = Gaussian()
+
+class Test_Gaussian_simple:
+
+    def test_initialization(self) -> None: 
+        assert gauss.mean == 25
+        assert gauss.stdev == 2
+
+    def test_pdf(self) -> None:
+        assert round(gauss.pdf(25), 5) == 0.19947
+
+    def test_mean_calculation(self) -> None:
+        euler.read_data_file('numbers.txt')
+        assert euler.calculate_mean() == sum(euler.data)/float(len(euler.data))
+
+    def test_stdev_calculation(self) -> None:
+        euler.read_data_file('numbers.txt', True)
+        assert round(euler.calculate_stdev(), 2) == 92.87
+        euler.read_data_file('numbers.txt', False)
+        assert round(euler.calculate_stdev(sample = False), 2) == 88.55
+                

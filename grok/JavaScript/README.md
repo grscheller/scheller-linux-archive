6f4439cf7419e992fc1cc2544656e26c316ce006
grscheller <geoffrey@scheller.com>
2025-03-17 16:04:44 -0600

    Tested typescript-tools formatter on old piece of code
    - grok/JavaScript/node-install/app.js
      - just changed single to double quotes

--- grok/JavaScript/README.md
+++
@@ -1,7 +0,0 @@
-# Learn the Go Language
-
-* Both an interpreter and compiler
-* Simplified C like syntax
-
-1. [Node Install](node-install)
-1. Hooray for Hollywood
--- grok/JavaScript/node-install/app.js
+++ grok/JavaScript/node-install/app.js
@@ -1,12 +1,12 @@
-const http = require('http');
+const http = require("http");
 
-const hostname = '192.168.1.22';
+const hostname = "192.168.1.22";
 const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello World");
 });
 
 server.listen(port, hostname, () => {

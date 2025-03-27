a8bec0b0ec56b5cfc4520633418db458906b0e10
grscheller <geoffrey@scheller.com>
2025-03-27 15:40:34 -0600

    Added a .editorconfig file
    - curious how GitHub style code blocks for go will be handled
      - set `indent_size = 4` for go
      - set `indent_size = 2` for markdown

--- grok/Go/babysteps/bomb/bomb.go
+++ grok/Go/babysteps/bomb/bomb.go
@@ -6,18 +6,16 @@ import (
 )
 
 func main() {
-	/*
-	   "select" statements choose which set of send or receive
-	   operations will proceed.  The Go Language random selects
-	   amoung the communications channels which are ready to run
-	*/
+	/* The `select` statement chooses which set of send or receive
+	   operations will proceed. The Go Language random selects among the
+	   communications channels which are ready to run.
 
+	   Not to be confused with the "switch" statement.
+	*/
 	tick := time.Tick(100 * time.Millisecond)
 	boom := time.After(700 * time.Millisecond)
 
 	for {
-		// Not to be confused with a "switch" statement
-		//   "switch" statements choose values in a definite order
 		select {
 		case <-tick:
 			fmt.Println("tick.")
